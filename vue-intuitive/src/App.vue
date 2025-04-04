c1475e1551c6c32d3c21b25179097e2cf3e5c924
Igor <igorb@alfamaweb.com.br>
2025-04-04 00:35:38 -0300

    First Commit

---
+++ vue-intuitive/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <OperadoraSearch />
+  </div>
+</template>
+
+<script>
+import OperadoraSearch from "./components/BuscarOperadora.vue";
+
+export default {
+  components: { OperadoraSearch },
+};
+</script>
---
+++ vue-intuitive/src/components/BuscarOperadora.vue
@@ -0,0 +1,212 @@
+<template>
+  <div class="container">
+    <div class="svg-container">
+      <svg
+        preserveAspectRatio="xMidYMid meet"
+        data-bbox="0 0.02 1242.09 287.41"
+        viewBox="0 0.02 1242.09 287.41"
+        xmlns="http://www.w3.org/2000/svg"
+        data-type="shape"
+        role="presentation"
+        aria-hidden="true"
+        aria-label=""
+      >
+        <g>
+          <path fill-rule="evenodd" d="M799.91 100.72h23.34v182.25h-23.34V100.72z"></path>
+          <path
+            d="m1216.13 222.56 19.79 10.41c-6.52 12.64-14.01 22.88-22.54 30.66-8.52 7.72-18.08 13.67-28.72 17.73-10.7 4.06-22.71 6.06-36.15 6.06-29.86 0-53.14-9.72-69.96-29.17-16.82-19.45-25.23-41.42-25.23-65.9 0-23.05 7.15-43.65 21.39-61.67 18.02-22.94 42.22-34.44 72.48-34.44s56.12 11.78 74.71 35.29c13.21 16.59 19.96 37.3 20.19 62.18h-164.75c.46 21.11 7.26 38.38 20.36 51.83 13.16 13.5 29.4 20.25 48.68 20.25 9.32 0 18.42-1.6 27.23-4.81 8.81-3.2 16.3-7.44 22.48-12.76 6.18-5.32 12.87-13.84 20.02-25.68Zm.23-49.08c-3.15-12.53-7.72-22.48-13.73-29.98-6.06-7.49-14.01-13.5-23.91-18.08-9.9-4.58-20.31-6.86-31.18-6.86-18.02 0-33.46 5.72-46.39 17.22-9.5 8.41-16.59 20.94-21.45 37.7h136.66Z"
+            fill-rule="evenodd"
+          ></path>
+          <path
+            fill-rule="evenodd"
+            d="M854.88 100.72h24.54l61.38 133.05 60.93-133.05h25.11l-83.41 182.25h-4.46l-84.09-182.25z"
+          ></path>
+          <path
+            d="M707.3 32.76h23.34v67.96h37.07v20.25h-37.07v162H707.3V32.76Z"
+            fill-rule="evenodd"
+          ></path>
+          <path fill-rule="evenodd" d="M614.91 100.72h23.34v182.25h-23.34V100.72z"></path>
+          <path
+            d="M626.52 25.89c5.32 0 9.84 1.89 13.61 5.72 3.78 3.78 5.66 8.41 5.66 13.73s-1.89 9.84-5.66 13.61c-3.78 3.83-8.29 5.72-13.61 5.72s-9.72-1.89-13.5-5.72c-3.78-3.78-5.66-8.35-5.66-13.61s1.89-9.95 5.66-13.73c3.78-3.83 8.24-5.72 13.5-5.72Z"
+            fill-rule="evenodd"
+          ></path>
+          <path
+            d="M406.63 100.72h23.34v85.06c0 20.76 1.14 35.01 3.37 42.85 3.32 11.15 9.78 19.96 19.22 26.43 9.44 6.46 20.77 9.73 33.87 9.73s24.2-3.15 33.35-9.44c9.1-6.35 15.45-14.59 18.88-24.88 2.35-7.04 3.55-21.91 3.55-44.68v-85.07h24.03v89.35c0 25.11-2.92 43.99-8.81 56.69-5.84 12.76-14.7 22.71-26.49 29.86-11.78 7.21-26.54 10.81-44.33 10.81s-32.61-3.6-44.5-10.81c-11.9-7.15-20.82-17.22-26.66-30.09-5.89-12.87-8.81-32.26-8.81-58.12v-87.69Z"
+            fill-rule="evenodd"
+          ></path>
+          <path
+            d="M313.68 32.76h23.34v67.96h37.07v20.25h-37.07v162h-23.34V32.76Z"
+            fill-rule="evenodd"
+          ></path>
+          <path
+            d="M84.12 100.72h23.34v32.66c9.44-12.47 19.85-21.79 31.23-27.92 11.44-6.12 23.85-9.21 37.3-9.21s25.8 3.43 36.38 10.35c10.58 6.92 18.42 16.25 23.45 27.97 5.03 11.67 7.55 29.92 7.55 54.69v93.7h-23.34v-86.84c0-20.94-.92-34.95-2.69-41.99-2.86-12.01-8.12-21.05-15.85-27.17-7.72-6.06-17.79-9.1-30.2-9.1-14.24 0-27 4.69-38.21 14.07-11.27 9.32-18.71 20.94-22.25 34.78-2.23 9.04-3.38 25.51-3.38 49.48v66.76H84.11V100.72Z"
+            fill-rule="evenodd"
+          ></path>
+          <path
+            d="m1036.99 67.04 6.88 3.63c-2.25 4.45-4.87 8.02-7.82 10.75-2.97 2.73-6.31 4.81-10 6.23-3.71 1.42-7.9 2.13-12.57 2.13-10.38 0-18.48-3.39-24.32-10.2s-8.76-14.49-8.76-23.07c0-8.06 2.47-15.26 7.42-21.57 6.26-8.02 14.66-12.05 25.18-12.05s19.49 4.13 25.96 12.35c4.61 5.81 6.94 13.05 7.02 21.73h-57.16c.14 7.42 2.51 13.49 7.06 18.22 4.57 4.73 10.2 7.1 16.9 7.1 3.23 0 6.38-.56 9.44-1.7 3.05-1.14 5.67-2.63 7.8-4.51 2.13-1.86 4.47-4.89 6.96-9.06Zm0-17.12c-1.1-4.37-2.69-7.86-4.77-10.45-2.1-2.61-4.85-4.73-8.28-6.32a25.22 25.22 0 0 0-10.81-2.39c-6.23 0-11.59 2.02-16.06 6.03-3.27 2.91-5.75 7.3-7.42 13.15H1037Z"
+            fill-rule="evenodd"
+          ></path>
+          <path
+            d="M944.33 24.54h8.36v9.3c2.49-3.65 5.13-6.38 7.88-8.2 2.77-1.82 5.65-2.73 8.66-2.73 2.25 0 4.67.74 7.24 2.17l-4.27 6.9c-1.72-.76-3.15-1.12-4.33-1.12-2.71 0-5.35 1.12-7.88 3.35-2.53 2.25-4.47 5.73-5.79 10.44-1.02 3.63-1.52 10.95-1.52 21.99v21.51h-8.36V24.54Z"
+            fill-rule="evenodd"
+          ></path>
+          <path
+            d="M929 24.54v63.61h-8.06V77.22c-3.43 4.17-7.28 7.3-11.55 9.42-4.27 2.09-8.94 3.15-14.01 3.15-9 0-16.68-3.27-23.07-9.8-6.36-6.52-9.56-14.47-9.56-23.82s3.23-16.98 9.66-23.48c6.42-6.5 14.17-9.78 23.2-9.78 5.23 0 9.94 1.12 14.17 3.33 4.23 2.23 7.94 5.57 11.15 10V24.55h8.06Zm-32.74 6.27c-4.55 0-8.76 1.12-12.63 3.35-3.85 2.26-6.92 5.39-9.2 9.46-2.27 4.05-3.41 8.34-3.41 12.85s1.14 8.78 3.43 12.87c2.31 4.09 5.39 7.26 9.28 9.56 3.87 2.27 8.04 3.41 12.47 3.41s8.74-1.14 12.75-3.39c4.01-2.26 7.1-5.31 9.26-9.18 2.17-3.85 3.25-8.2 3.25-13.03 0-7.36-2.43-13.53-7.28-18.48-4.85-4.95-10.81-7.42-17.92-7.42Z"
+            fill-rule="evenodd"
+          ></path>
+          <path
+            d="m849.73 18.12-6.78 5.21c-3.73-4.87-8.22-8.56-13.47-11.07s-10.99-3.77-17.28-3.77c-6.86 0-13.21 1.64-19.05 4.93-5.85 3.29-10.38 7.72-13.59 13.27-3.21 5.57-4.83 11.81-4.83 18.74 0 10.5 3.59 19.23 10.79 26.26 7.18 7 16.26 10.51 27.21 10.51 12.03 0 22.11-4.71 30.21-14.15l6.78 5.15c-4.29 5.45-9.64 9.68-16.04 12.65-6.4 2.99-13.57 4.47-21.49 4.47-15.04 0-26.9-5.01-35.59-15.02-7.28-8.44-10.93-18.66-10.93-30.63 0-12.59 4.41-23.16 13.25-31.76C787.74 4.31 798.79.02 812.08.02c8.04 0 15.28 1.6 21.75 4.77 6.46 3.17 11.77 7.62 15.9 13.35Z"
+            fill-rule="evenodd"
+          ></path>
+          <path fill-rule="evenodd" d="M7.55 100.72h23.34v182.25H7.55V100.72z"></path>
+          <path
+            d="M19.16 25.89c5.32 0 9.84 1.89 13.61 5.72 3.78 3.78 5.66 8.41 5.66 13.73s-1.89 9.84-5.66 13.61c-3.78 3.83-8.29 5.72-13.61 5.72s-9.72-1.89-13.5-5.72C1.88 55.17 0 50.6 0 45.34s1.89-9.95 5.66-13.73c3.78-3.83 8.24-5.72 13.5-5.72Z"
+            fill-rule="evenodd"
+          ></path>
+          <path
+            d="M810.96 25.89c5.32 0 9.84 1.89 13.61 5.72 3.78 3.78 5.66 8.41 5.66 13.73s-1.89 9.84-5.66 13.61c-3.78 3.83-8.29 5.72-13.61 5.72s-9.72-1.89-13.5-5.72c-3.78-3.78-5.66-8.35-5.66-13.61s1.89-9.95 5.66-13.73c3.78-3.83 8.24-5.72 13.5-5.72Z"
+            fill-rule="evenodd"
+          ></path>
+        </g>
+      </svg>
+    </div>
+  </div>
+  <div class="p-6 max-w-4xl mx-auto">
+    <h1 class="text-2xl font-bold mb-4">Consulta de Operadoras</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
+      <input
+        v-model="search.registro"
+        placeholder="Registro ANS"
+        class="p-2 border rounded w-full"
+      />
+      <input v-model="search.cnpj" placeholder="CNPJ" class="p-2 border rounded w-full" />
+      <input v-model="search.cidade" placeholder="Cidade" class="p-2 border rounded w-full" />
+      <input v-model="search.uf" placeholder="UF" class="p-2 border rounded w-full" />
+      <input v-model="search.modalidade" placeholder="Modalidade" class="p-2 border rounded w-full"/>
+    </div>
+    <button @click="buscarOperadoras" class="bg-blue-500 text-white px-4 py-2 rounded w-full">
+      Buscar
+    </button>
+    <div v-if="loading" class="mt-4">Carregando...</div>
+    <div v-if="error" class="mt-4 text-red-500">{{ error }}</div>
+    <div class="resultado-count text-center mt-4 text-green-500">{{ operadoras.length }} Operadoras encontradas</div>
+    <div v-if="operadoras.length" class="grid-response">
+      <div class="card" v-for="(operadora, index) in operadoras" :key="index">
+        <p><strong>Registro ANS:</strong> {{ operadora.registro_ans }}</p>
+        <p><strong>Razao Social:</strong> {{ operadora.razao }}</p>
+        <p><strong>CNPJ:</strong> {{ operadora.cnpj }}</p>
+        <p><strong>Cidade:</strong> {{ operadora.cidade }}</p>
+        <p><strong>UF:</strong> {{ operadora.uf }}</p>
+        <p><strong>Modalidade:</strong> {{ operadora.modalidade }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      search: {
+        registro: '',
+        cnpj: '',
+        cidade: '',
+        uf: '',
+        modalidade: '',
+      },
+      operadoras: [],
+      loading: false,
+      error: '',
+    }
+  },
+  methods: {
+    async buscarOperadoras() {
+      this.loading = true
+      this.error = ''
+      this.operadoras = []
+
+      let url = 'http://127.0.0.1:8000/buscar'
+      let params = new URLSearchParams()
+
+      Object.keys(this.search).forEach((key) => {
+        if (this.search[key]) {
+          params.append(key, this.search[key])
+        }
+      })
+
+      if (params.toString()) {
+        url += `?${params.toString()}`
+      }
+
+      try {
+        const response = await axios.get(url)
+        this.operadoras = response.data.map((op) => ({
+          razao: op.Razao_Social,
+          registro_ans: op.Registro_ANS,
+          cnpj: op.CNPJ,
+          cidade: op.Cidade,
+          uf: op.UF,
+          modalidade: op.Modalidade,
+        }))
+      } catch (err) {
+        this.error = 'Erro ao buscar operadoras. Verifique os filtros e tente novamente.'
+      } finally {
+        this.loading = false
+      }
+    },
+  },
+}
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+}
+h1 {
+    color: #696969;
+}
+p {
+    color: #696969;
+}
+input {
+    color: #696969;
+}
+.container {
+  text-align: center;
+  padding: 20px;
+}
+
+.svg-container {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+.svg-container svg {
+  max-width: 200px;
+  height: auto;
+}
+
+.grid-response {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  gap: 20px;
+  margin-top: 20px;
+}
+
+.card {
+  background-color: #f4f4f4;
+  padding: 15px;
+  border-radius: 10px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  text-align: left;
+}
+
+@media (max-width: 768px) {
+    .grid-response {
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    }
+}
+</style>
